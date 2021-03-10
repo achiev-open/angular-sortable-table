c3730d53d69319fdcd56e5dab8a9b978b5581ab3
mel-mouk <contact@achiev.fr>
2021-03-10 17:02:59 +0100

    Simple sortable list by drag n drop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-sortable-table';
+  public items: Array<string> = ['Apple', 'Banana', 'Orange', 'Peach', 'Kiwi'];
+  private draggedIndex: number;
+  public draggedOverIndex: number;
+
+  allowDrop($event, index): void {
+    this.draggedOverIndex = index;
+    $event.preventDefault();
+  }
+
+  onDragStart(index): void {
+    this.draggedIndex = index;
+  }
+
+  onDrop($event, index): void {
+    $event.preventDefault();
+    const item = this.items[this.draggedIndex];
+    this.items.splice(this.draggedIndex, 1);
+    this.items.splice(index, 0, item);
+    this.draggedIndex = -1;
+    this.draggedOverIndex = -1;
+  }
 }
